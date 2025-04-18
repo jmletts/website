1fbed71a4922047901d4555ac9493e48073e6d24
joaquin <joaquir520@live.com>
2025-04-17 23:25:19 -0500

    updt

--- src/app/dashboard/components/side-nav/side-nav.component.ts
+++ src/app/dashboard/components/side-nav/side-nav.component.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss'
 })
 export class SideNavComponent {
+    submenu = {
+      Acct : false,
+      Dash: false,
+      In: false,
+      Prod: false,
+      Shp: false,
+      One: false,
+      Std: false,
+      Sett: false,
+      Supp: false
+    };
 
+hasOpen(control : string) {
+  (Object.keys(this.submenu) as (keyof typeof this.submenu)[]).forEach(key => {
+    if (key === control) {
+      if (this.submenu[key] === true) {
+        this.submenu[key] = false;
+      } else {
+        this.submenu[key] = true;
+      }
+    } 
+    else {
+      this.submenu[key] = false;
+    } 
+  });
+}
 }
